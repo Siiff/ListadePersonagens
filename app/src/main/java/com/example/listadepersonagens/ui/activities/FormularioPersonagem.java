87741883a810ffa8a92fd5004191c20161cef8ba
Siff <ricardodsgenari@gmail.com>
2021-03-17 21:24:05 -0300

    formulario

--- app/src/main/java/com/example/listadepersonagens/ui/activities/FormularioPersonagem.java
+++ app/src/main/java/com/example/listadepersonagens/ui/activities/FormularioPersonagem.java
@@ -1,13 +1,14 @@
-package com.example.listadepersonagens;
+package com.example.listadepersonagens.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class Formulario extends AppCompatActivity {
+public class FormularioPersonagem extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_formulario);
+        setContentView(R.layout.activity_formulario_personagem);
     }
 }
