e9a9c2705195bdd2461a33f492381efccce17db9
Siff <ricardodsgenari@gmail.com>
2021-03-17 21:17:28 -0300

    formulario

---
+++ app/src/main/java/com/example/listadepersonagens/Formulario.java
@@ -0,0 +1,13 @@
+package com.example.listadepersonagens;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Formulario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+    }
+}
