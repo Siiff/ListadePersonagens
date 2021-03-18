d583f24fe996f811b8cffa0aa356993510aa8915
Siff <ricardodsgenari@gmail.com>
2021-03-17 21:27:58 -0300

    formulario

--- app/src/main/java/com/example/listadepersonagens/ui/activities/FormularioPersonagem.java
+++ app/src/main/java/com/example/listadepersonagens/ui/activities/FormularioPersonagem.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.listadepersonagens.R;
+
 public class FormularioPersonagem extends AppCompatActivity {
 
     @Override
