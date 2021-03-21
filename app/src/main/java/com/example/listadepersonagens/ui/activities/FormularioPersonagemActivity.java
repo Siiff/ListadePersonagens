c5f4b7b425adac5843398d9821944923ee3deb55
Siff <ricardodsgenari@gmail.com>
2021-03-21 20:27:10 -0300

    comentário

--- app/src/main/java/com/example/listadepersonagens/ui/activities/FormularioPersonagemActivity.java
+++ app/src/main/java/com/example/listadepersonagens/ui/activities/FormularioPersonagemActivity.java
@@ -3,6 +3,9 @@ package com.example.listadepersonagens.ui.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.listadepersonagens.R;
 
@@ -12,5 +15,15 @@ public class FormularioPersonagemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_personagem);
+
+        Button botaoEnviar = findViewById(R.id.botaoEnviar);
+        botaoEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //checando se o botão Enviar esta funcionando e criando sua função através do java//
+                Toast.makeText(FormularioPersonagemActivity.this,"Estou funcionando", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
